d80426e9566f63f5da077bd1fa6e67c93e519b63
Denys Rógeres <dennysrogeres@gmail.com>
2022-06-19 02:28:19 -0300

    terminando header

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,20 @@ import { makeStyles } from '@mui/styles';
 import { AppBar, IconButton, Toolbar, Button } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AppsIcon from '@mui/icons-material/Apps';
 
 
 const useStyles = makeStyles({
     root: {
         height: '100vh',
+    },
+    icons: {
+        paddingRight: 5,
+    },
+    iconYoutube: {
+        height: 30
     },
     appBar: {
         boxShadow: 'none',
@@ -32,7 +41,21 @@ export function Home() {
                     >
                         <MenuIcon />
                     </IconButton>
+                    <img className={classes.iconYoutube} src="../public/images/preto.png" alt="Logo Youtube" />
                     <div className={classes.grow}/>
+
+                    <IconButton className={classes.icons} color='inherit'>
+                        <VideoCallIcon />
+                    </IconButton>
+
+                    <IconButton className={classes.icons} color='inherit'>
+                        <AppsIcon />
+                    </IconButton>
+
+                    <IconButton className={classes.icons} color='inherit'>
+                        <MoreVertIcon />
+                    </IconButton>
+
                    <Button startIcon={<AccountCircleIcon />}variant="outlined" color='primary'>
                        FAZER LOGIN
                    </Button>
